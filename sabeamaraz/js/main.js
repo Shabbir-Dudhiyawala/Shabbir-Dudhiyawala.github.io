a0775c6bbb50c9e828da4445f052b4338965fbae
Shabbir Dudhiyawala <66951720+shabbirnd@users.noreply.github.com>
2023-06-16 20:40:36 +0530

    Add files via upload

---
+++ sabeamaraz/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  // Listen for the 'keydown' event to detect the Enter key press
+  barcodeInput.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      const value = event.target.value;
+      // Perform your desired action here
+      sendScannedValueToAPI(value);
+    }
+  });
+  // Function to send the scanned value to the Google Apps Script API
+  function sendScannedValueToAPI(value) {
+    console.log(value);
+    const url = `https://script.google.com/macros/s/AKfycbyfTqbjFndXu9O76by-YVxF-cRrYLHCeIgIK8-xCFxJqHQ2EuXt9TkMOBiHezbFHwG-/exec?its=${value}`;
+
+    //   Make the AJAX request
+    $.get(url, function (data, status) {
+      console.log(status);
+      console.log(data);
+    });
+  }
+});
