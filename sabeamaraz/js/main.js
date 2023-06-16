8f2989043c8e3abb3ac89015c03d82c438d75514
Shabbir Dudhiyawala <66951720+shabbirdudhiya@users.noreply.github.com>
2023-06-16 21:07:15 +0530

    Update main.js

--- sabeamaraz/js/main.js
+++ sabeamaraz/js/main.js
@@ -3,18 +3,25 @@ $(document).ready(function () {
   barcodeInput.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
       const value = event.target.value;
+
       // Perform your desired action here
       sendScannedValueToAPI(value);
+      barcodeInput.value = "";
+      showRazaMsg();
     }
   });
   // Function to send the scanned value to the Google Apps Script API
   function sendScannedValueToAPI(value) {
-    console.log(value);
     const url = `https://script.google.com/macros/s/AKfycbyfTqbjFndXu9O76by-YVxF-cRrYLHCeIgIK8-xCFxJqHQ2EuXt9TkMOBiHezbFHwG-/exec?its=${value}`;
-
     //   Make the AJAX request
-    $.get(url, function (data, status) {
-         barcodeInput.value = "";
-    });
+    $.get(url, function (data, status) {});
+  }
+  function showRazaMsg() {
+    // Change the value
+    let razaMsg = document.getElementById("razaMsg");
+    // Revert the value after 3 seconds
+    setTimeout(function () {
+      razaMsg.innerHTML = "Mubarak! You have been granted raza!";
+    }, 3000);
   }
 });
